4e734e492ccfd5d145a63680f1cef088c53dbf00
123zmz123 <1245539137@qq.com>
2023-05-04 13:42:22 +0800

    cmd2 framework prove can be run. tc27D infra file added

--- py/src/cli.py
+++ py/src/cli.py
@@ -1,11 +1,62 @@
 from cmd2 import Cmd
+from cmd2 import style
+from cmd2 import Fg,Bg
+from core import ParserReg,Calculator
+from pathlib import Path
+import os
 
 class App(Cmd):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.cc = style("this is the command line tool of calculator register content",
+                           fg=Fg.RED,bg=Bg.WHITE,bold=True)
+        self.cnf_no_exist_msg = style("cnf folder were not exist ./cnf or ../cnf where is it? pls check you setup",fg=Fg.RED,bold=True)
 
+        self.cnf_no_exist_msg = style("cnf folder were not exist ./cnf or ../cnf where is it? pls check you setup",fg=Fg.RED,bold=True)
+
+        self.intro = style("workflow\n \
+                           1. list configuration file by lscnf\n \
+                           2. load configuration file by ldcnf file_name\n \
+                           3. fuzzy search register name by sech register_name\n \
+                           4. choose the register and use calc register_name rawvalue to see the field value\n\
+                            4.1 example input calc FUE 0x19890604\n \
+                           ",fg=Fg.GREEN,bold=True)
+        
+        self.parser = ParserReg()
+        self.calculator = Calculator()
+        self.cnf_dir =None
+        
+    # list configuration file command
+    def do_lscnf(self,_):
+        script_path = Path(__file__).parent
+        # lev1 and lev2 means we have 2 way to store configuration files
+        lev1_path = script_path.joinpath("cnf")
+        lev2_path = script_path.parent.joinpath("cnf")
+
+        if(lev1_path.exists()):
+            self.cnf_dir = lev1_path
+            for item in os.listdir(lev1_path):
+                print(item)
+        elif(lev2_path.exists()):
+            self.cnf_dir = lev2_path
+            for item in os.listdir(lev2_path):
+                print(item)
+        else:
+            self.poutput(self.cnf_no_exist_msg)
+
+    # load configuration command
+    def do_ldcnf(self,arg):
+        print(arg)
+        pass
+    
+    # fuzzy search command
+    def do_fuzzy(self,arg):
+        pass
+    # calc command 
+    def do_calc(self,arg):
+        pass
+        
+    
 if __name__ == '__main__':
     app = App()
     app.cmdloop()
-    
-    
-    
